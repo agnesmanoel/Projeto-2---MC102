d41179403a0e59b54098a6c90eec5eeb46c8cdc2
agnesmanoel <a232879@dac.unicamp.br>
2024-06-07 18:33:27 -0300

    estrategia marlom

--- student_players.py
+++ student_players.py
@@ -1,95 +1,77 @@
 from basic_players import Player
-from judge import Judge
 
-class Jogador(Player):
-
-    def contar_peças_inicial(self): # Conta as peças no inicio do jogo
-        my_tiles = self.tiles
-        peças = {1:11, 2:11, 3:11, 4:11, 5:11, 6:11, 7:11, 8:11, 9:11}
-        for i,j in my_tiles:
-            for k in peças:
-                if i == k:
-                    peças[k] -= 1
-                elif j == k:
-                    peças[k] -= 1
-        return peças
-
+class Xerecuda(Player):
     def play(self, board_extremes, play_hist):
-        
-        def estrategia_agnes(self):
-            last_play = play_hist[0]
-            contagem_peças = self.contar_peças()
-            for k in contagem_peças:
-                for i,j in last_play:
-                    if i == k:
-                        contagem_peças[i] -= 1
-                    if j == k: 
-                        contagem_peças[i] -= 1
-            return contagem_peças
-        
-        def peças_ignoradas(self):
-            #como armazenar o valor desse dic - quero que meu dic conte todas a vezes que uma peça for pulada,
-            pass
-
-
-
-
         playable_tiles = self._tiles
-        if len(board_extremes) > 0:
-            # Filtrando as peças jogáveis
-            playable_tiles = [tile for tile in self._tiles if tile[0] in board_extremes or tile[1] in board_extremes]
-        else:
-            return 1, None
 
-        # Verificação de quantas peças com extremidades iguais.
-        contagem = {}
-        for p in playable_tiles:
-            for n in p:
-                if n in contagem:
-                    contagem[n] += 1
-                else:
-                    contagem[n] = 1
-
-        k_rep = None
-        ocor = 0
-        if contagem:
-            k_rep = max(contagem, key=contagem.get) # Número que mais se repete
-            ocor = contagem[k_rep] # Ocorrências da repetição
-
-        # Verifica se há mais de duas peças com o mesmo número.
-        if ocor > 2:
-            # Armazena as peças de extremidade igual a 'k_rep'.
-            pecas_k = []
+        if len(board_extremes) == 0:  # Condição para saber se o jogador é o primeiro a jogar.
+            # Implementar uma estratégia para quando se começa primeiro.
             for tile in playable_tiles:
-                if tile[0] == k_rep or tile[1] == k_rep:
-                    pecas_k.append(tile)
+                if tile[0] == tile[1]:
+                    return 1, tile
+                else:
+                    # Caso não encontre uma peça dupla, prossegue com a lógica abaixo.
+                    contagem = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0}
+                    for i, j in playable_tiles:
+                        contagem[i] += 1
+                        contagem[j] += 1
 
-            menor_soma = pecas_k[0][0] + pecas_k[0][1]
-            menor_peca = pecas_k[0]
-            for tile in pecas_k:
-                if tile[0] + tile[1] < menor_soma:
-                    menor_soma = tile[0] + tile[1]
-                    menor_peca = tile
-            return 1, menor_peca
-        else:
-            highest = -1
-            tile_sum = -1
-            for i in range(len(playable_tiles)):
-                if playable_tiles[i][0] + playable_tiles[i][1] > tile_sum:
-                    tile_sum = playable_tiles[i][0] + playable_tiles[i][1]
-                    highest = i
-            if highest >= 0:
-                return 1, playable_tiles[highest]
+                    max_rep = max(contagem, key=contagem.get)
+                    ocor = contagem[max_rep]
+                    pecas_max = [tile for tile in playable_tiles if tile[0] == max_rep or tile[1] == max_rep]
+
+                    maior_soma = 0
+                    maior_tile = None
+
+                    for tile in pecas_max:
+                        if tile[0] + tile[1] > maior_soma:
+                            maior_soma = tile[0] + tile[1]
+                            maior_tile = tile
+                    return 1, maior_tile
+
+        if len(board_extremes) > 0:
+            playable_tiles = [tile for tile in self._tiles if tile[0] in board_extremes or tile[1] in board_extremes]
+
+            contagem = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0}
+            for i, j in playable_tiles:
+                contagem[i] += 1
+                contagem[j] += 1
+
+            max_rep = max(contagem, key=contagem.get)
+            ocor = contagem[max_rep]
+
+            # Verifica se há mais de duas peças com o mesmo número.
+            if ocor > 4:
+                peças_k = []
+                for tile in playable_tiles:
+                    if tile[0] == max_rep or tile[1] == max_rep:
+                        peças_k.append(tile)
+                
+                menor_soma = peças_k[0][0] + peças_k[0][1]
+                menor_peça = peças_k[0]
+
+                for tile in peças_k:
+                    if tile[0] + tile[1] < menor_soma:
+                        menor_soma = tile[0] + tile[1]
+                        menor_peça = tile
+
+                return 1, menor_peça
             else:
-                return 1, None
-        
+                highest = -1
+                tile_sum = -1
+                for i in range(len(playable_tiles)):
+                    if playable_tiles[i][0] + playable_tiles[i][1] > tile_sum:
+                        tile_sum = playable_tiles[i][0] + playable_tiles[i][1]
+                        highest = i
+                if highest >= 0:
+                    return 1, playable_tiles[highest]
+                else:
+                    return 1, None
 
 # Função que define o nome da dupla:
 def pair_name():
-    return "BONECAS"
+    return "BONECAS da COMP"
 
 # Função que cria a dupla:
 def create_pair():
-    pass
-
-    #return (Agnes("123", 'Agnes'), Marlom('321', 'Marlon'))  # Defina aqui a dupla de jogadores. Deve ser uma tupla com dois jogadores.
+    return (Xerecuda("123", 'Agnes'), Xerecuda('321', 'Marlon'))  # Defina aqui a dupla de jogadores. Deve ser uma tupla com dois jogadores.
